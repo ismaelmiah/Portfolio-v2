af99184931a2505c5ab60d3243b9ce5a314119e0
ismailhosen <i.miah@asthait.com>
2023-08-06 11:52:33 +0600

    Layout designed

---
+++ src/layout.jsx
@@ -0,0 +1,21 @@
+import About from "./About";
+import Experience from "./Experience";
+import Header from "./Header";
+
+const layout = () => {
+  return (
+    <>
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <div className="lg:flex lg:justify-between lg:gap-4">
+          <Header />
+          <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
+            <About />
+            <Experience />
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default layout;
