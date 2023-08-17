5ce3ba5e9457880022478eb33390cbcbc108b8f5
ismailhosen <i.miah@asthait.com>
2023-08-17 21:18:45 +0600

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
-import Layout from './layout';
+import "./App.css";
+import ArchiveProjects from "./ArchiveProjects";
+import Layout from "./layout";
+import ErrorPage from "./ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/archive",
+    element: <ArchiveProjects />,
+  },
+]);
 
 const App = () => {
   return (
-    <Layout></Layout>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
-}
+};
 
 export default App;
