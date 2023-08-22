fb5887ac51bcd59a7c3808a98d6fc85006b10cfd
Ismael Miah <ismail27.dec@gmail.com>
2023-08-22 21:47:20 +0600

    About section added

--- src/About.jsx
+++ src/About.jsx
@@ -13,74 +13,41 @@ const About = () => {
         </div>
         <div>
           <p className="mb-4">
-            Back in 2012, I decided to try my hand at creating custom Tumblr
-            themes and tumbled head first into the rabbit hole of coding and web
-            development. Fast-forward to today, and I’ve had the privilege of
-            building software for an{" "}
+            Back in 2016, I decided to try my hand at creating Facebook on my
+            own with raw PHP and that was the moment I fell into coding and web
+            development. Fast-forward to today, and I've had the privilege of
+            building software for {" "}
             <a
               className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-              href="https://us.mullenlowe.com/"
+              href="https://devskill.com/"
               target="_blank"
               rel="noreferrer"
             >
-              advertising agency
+              DevSkill
             </a>
-            , a{" "}
+            , and{" "}
             <a
               className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-              href="https://starry.com/"
+              href="https://www.asthait.com/"
               target="_blank"
               rel="noreferrer"
             >
-              start-up
-            </a>
-            , a{" "}
-            <a
-              className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-              href="https://scout.camd.northeastern.edu/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              student-led design studio
-            </a>
-            , and a{" "}
-            <a
-              className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-              href="https://www.apple.com/apple-music/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              huge corporation
-            </a>
-            .
+              ASTHA-IT.
+            </a>            
           </p>
           <p className="mb-4">
-            My main focus these days is building products and leading projects
+          My main focus these days is building products and leading projects
             for our clients at{" "}
             <a
               className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-              href="https://upstatement.com/"
+              href="https://www.asthait.com/"
               target="_blank"
               rel="noreferrer"
             >
-              Upstatement
+              ASTHA-IT
             </a>
-            . In my free time I've also released an{" "}
-            <a
-              className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
-              href="https://www.newline.co/courses/build-a-spotify-connected-app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              online video course
-            </a>{" "}
-            that covers everything you need to know to build a web app with the
-            Spotify API.
-          </p>
-          <p>
-            When I’m not at the computer, I’m usually rock climbing, hanging out
-            with my wife and two cats, or running around Hyrule searching for
-            somethings
+            . In my free time, I contribute some open-source projects and try to
+            learn something new from LinkedIn connections.
           </p>
         </div>
       </section>
