5b6ab352f653e174fb9493a9c4614b0742c4a77e
Ismael Miah <ismail27.dec@gmail.com>
2023-08-26 00:00:46 +0600

    Project section added

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <>
       <section
         id="about"
-        className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+        className="mb-16 scroll-mt-16 md:mb-16 lg:mb-30 lg:scroll-mt-24"
         aria-label="About me"
       >
         <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
