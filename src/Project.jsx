232e9665d478418762a0532987d5eeeb07f37799
Ismael Miah <ismail27.dec@gmail.com>
2023-08-26 00:26:05 +0600

    Archive Projects added

--- src/Project.jsx
+++ src/Project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
     <>
       <section
         id="projects"
-        className="mb-10 scroll-mt-10 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+        className="scroll-mt-10 lg:scroll-mt-24"
         aria-label="Selected projects"
       >
         <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
