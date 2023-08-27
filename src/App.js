bcefea982a34d7bbc69ce41df71a43ee0a3fbf1b
Ismael Miah <ismail27.dec@gmail.com>
2023-08-27 23:06:07 +0600

    Added a focus event on mouseMove

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <>
+      <div
+        id="pointer-events"
+        className="pointer-events-none fixed inset-0 z-300 transition duration-300"
+        style={{
+          background:
+            "radial-gradient(600px at 750px 505px, rgba(29, 78, 216, 0.15), transparent 80%)",
+        }}
+      ></div>
       <RouterProvider router={router} />
     </>
   );
