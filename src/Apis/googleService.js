eaa7fb5cb97dca2493ba314e2664fe742235c043
Ismael Miah <ismail27.dec@gmail.com>
2023-08-30 12:00:00 +0600

    tailwindcss form plugin installed and login component added

---
+++ src/Apis/googleService.js
@@ -0,0 +1,12 @@
+import { google } from "googleapis"
+
+
+export const GoogleService = () => {
+    const GetData = () => {
+        console.log('Sheet Data loading');
+    }
+
+    const PostData = (body) => {
+        console.log(body);
+    }
+}
