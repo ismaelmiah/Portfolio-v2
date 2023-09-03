b689719956e7a298e05822fd39a1ec2a51d8f2fc
Ismael Miah <ismail27.dec@gmail.com>
2023-09-03 12:00:00 +0600

    dashboard layout redirection

---
+++ src/Components/Admin/analytics.jsx
@@ -0,0 +1,6 @@
+
+const Analytics = () => {
+    return (<></>);
+}
+
+export default Analytics;
--- src/Components/Admin/login.jsx
+++ src/Components/Admin/login.jsx
@@ -1,10 +1,20 @@
+
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    console.log('called');
+    navigate("/admin/dashboard");
+  }
+
   return (
     <>
       <div>
         <div className="login">
           <h1>Login</h1>
-          <form method="post">
+          <form method="post" onSubmit={handleSubmit}>
             <input
               type="email"
               className="mt-4 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
