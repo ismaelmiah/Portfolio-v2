5707d13e70a40b4c0a7ebda4d7e2632a08db867e
Ismael Miah <ismail27.dec@gmail.com>
2024-12-05 21:30:00 +0000

    linux and networking certificate added

--- pages/certifications/index.tsx
+++ pages/certifications/index.tsx
@@ -14,6 +14,49 @@ const Certifications = () => {
     setModalOpen(false);
   };
 
+  var certificates = [
+    {
+      title: 'Cloud Computing Fundamental',
+      fileName: 'cloud-computing-fundamentals.png'
+    },
+    {
+      title: 'Asp .NET Full Stack MVC Certificate',
+      fileName: 'asp.net-mvc-certificate.jpg'
+    },
+    {
+      title: 'HackerRank C# Basic Certificate',
+      fileName: 'basic-c-sharp-with-hackerrank.png'
+    },
+    {
+      title: 'Foundational C sharp Certificate(Microsoft)',
+      fileName: 'foundational-c-sharp-with-microsoft.png'
+    },
+    {
+      title: 'HackerRank SQL Basic Certificate',
+      fileName: 'basic-sql-with-hackerrank.png'
+    },
+    {
+      title: 'HackerRank JavaScript Basic Certificate',
+      fileName: 'basic-javascript-with-hackerrank.png'
+    },
+    {
+      title: 'FreeCodeCampg JavaScript Algorithms and Data Structure',
+      fileName: 'javascript-algorithms-and-data-structures.png'
+    },
+    {
+      title: 'Udemy AWS Course Completion Certificate',
+      fileName: 'udemy-aws-course-completion-certificate.jpg'
+    },
+    {
+      title: 'Linux Essential Course Completion Certificate',
+      fileName: 'linux-essential.png'
+    },
+    {
+      title: 'Network Essential Course Completion Certificate',
+      fileName: 'network-essential.png'
+    }
+  ]
+
   return (
     <>
       <section
@@ -27,119 +70,25 @@ const Certifications = () => {
           </h2>
         </div>
         <div className="w-full flex flex-wrap">
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/cloud-computing-fundamentals.png"
-              width={150}
-              height={200}
-              alt="Cloud Computing Fundamental"
-              onClick={() =>
-                handleOpenModal("/certificates/cloud-computing-fundamentals.png")
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/asp.net-mvc-certificate.jpg"
-              width={150}
-              height={200}
-              alt="Asp .NET Full Stack MVC Certificate"
-              onClick={() =>
-                handleOpenModal("/certificates/asp.net-mvc-certificate.jpg")
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/basic-c-sharp-with-hackerrank.png"
-              width={150}
-              height={200}
-              alt="HackerRank C# Basic Certificate"
-              onClick={() =>
-                handleOpenModal(
-                  "/certificates/basic-c-sharp-with-hackerrank.png"
-                )
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/foundational-c-sharp-with-microsoft.png"
-              width={150}
-              height={200}
-              alt="Foundational C sharp Certificate(Microsoft)"
-              onClick={() =>
-                handleOpenModal(
-                  "/certificates/foundational-c-sharp-with-microsoft.png"
-                )
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/basic-javascript-with-hackerrank.png"
-              width={150}
-              height={200}
-              alt="HackerRank JavaScript Basic Certificate"
-              onClick={() =>
-                handleOpenModal(
-                  "/certificates/basic-javascript-with-hackerrank.png"
-                )
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/basic-sql-with-hackerrank.png"
-              width={150}
-              height={200}
-              alt="HackerRank SQL Basic Certificate"
-              onClick={() =>
-                handleOpenModal("/certificates/basic-sql-with-hackerrank.png")
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/javascript-algorithms-and-data-structures.png"
-              width={150}
-              height={200}
-              alt="FreeCodeCampg JavaScript Algorithms and Data Structure"
-              onClick={() =>
-                handleOpenModal(
-                  "/certificates/javascript-algorithms-and-data-structures.png"
-                )
-              }
-            />
-          </div>
-          <div className="w-1/2 p-2">
-            <Image
-              className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
-              src="/certificates/udemy-aws-course-completion-certificate.jpg"
-              width={150}
-              height={200}
-              alt="Udemy AWS Course Completion Certificate"
-              onClick={() =>
-                handleOpenModal(
-                  "/certificates/udemy-aws-course-completion-certificate.jpg"
-                )
-              }
-            />
-          </div>
+          {certificates.map((certificate, index) => (
+            <div key={index} className="w-1/2 p-2">
+              <Image
+                className="w-full h-auto transition-transform duration-300 transform hover:scale-110 cursor-pointer"
+                src={`/certificates/${certificate.fileName}`}
+                width={150}
+                height={200}
+                alt={certificate.title}
+                onClick={() => handleOpenModal('/certificates/' + certificate.fileName)}
+              />
+            </div>
+          ))}
         </div>
       </section>
 
       {modalOpen && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 flex justify-center items-center z-50">
           <span
-            className="absolute top-4 right-4 text-white text-2xl cursor-pointer"
+            className="absolute top-4 right-4 text-white text-6xl cursor-pointer"
             onClick={() => handleCloseModal()}
           >
             &times;
