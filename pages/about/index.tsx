14ef38ef23b79f65238d4ba9e6e35b030fc863f5
Ismael Miah <29182508+ismaelmiah@users.noreply.github.com>
2025-08-14 13:11:33 +0100

    Update index.tsx

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -100,6 +100,11 @@ const About = () => {
             </li>
             <li className="mr-1.5 mt-2">
               <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                Angular
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
                 React
               </div>
             </li>
